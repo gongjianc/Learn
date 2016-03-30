bf6a05ac4d33ca30fb0e69fce566e5360d1c6a1a
JayGong <gongjian0309@gmail.com>
2016-03-30 04:56:38 -0700

    programer

---
+++ programer/1.5.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    char matrix[3][3] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}};
+    printf("%c\n", matrix[2][3]);
+    return 0;
+}
+    
+
---
+++ programer/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char ca1[] = {'C', '+', '+'};
+    printf("sizeof of ca1 is %d\n", (int)sizeof(ca1));
+    printf("strlen of ca1 is %d\n", (int)strlen(ca1));
+    return 0;
+}
