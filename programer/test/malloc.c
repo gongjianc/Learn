55f9a5aa1338a53dd93cdeb61c33c8a780010f07
JayGong <gongjian0309@gmail.com>
2016-04-01 08:32:52 -0700

    nothing to say

---
+++ programer/test/malloc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char *test_malloc()
+{
+    char *p = malloc(16);
+    p = "hello,world";
+    return p;
+}
+
+
+int main(void)
+{
+    printf("test_malloc: %s\n", test_malloc());
+    printf("test_not_malloc: %s\n", test_not_malloc());
+    return 0;
+}
